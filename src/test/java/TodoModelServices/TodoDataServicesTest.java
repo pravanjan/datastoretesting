d2b0fd59c7d7f4d0ee1ee44fcd702a0ae824cfb2
pravanjan <pravanjan.niranjan.anywhere.co>
2018-11-26 11:56:15 +0530

    add changes in datastore service test files

--- src/test/java/TodoModelServices/TodoDataServicesTest.java
+++ src/test/java/TodoModelServices/TodoDataServicesTest.java
@@ -1,1 +1,50 @@
-package TodoModelServices;import com.google.appengine.api.datastore.EntityNotFoundException;import com.google.appengine.api.datastore.Key;import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;import com.google.appengine.tools.development.testing.LocalServiceTestHelper;import org.junit.AfterClass;import static org.junit.Assert.assertEquals;import org.junit.BeforeClass;import org.junit.Test;public class TodoDataServicesTest {    private static final LocalServiceTestHelper helper =            new LocalServiceTestHelper(                    new LocalDatastoreServiceTestConfig()                            .setDefaultHighRepJobPolicyUnappliedJobPercentage(0));    private static final TodoDataServices dataServices = new TodoDataServices();    static  Key key = null;    @BeforeClass    public static void  setUpClass() {        helper.setUp();          key = dataServices.saveTask("daily","dailyKey");    }    @AfterClass    public static void tearDown() {        helper.tearDown();    }  @Test    public void saveTask_valid_type (){      assertEquals("dailyKey",key.getName());  }    @Test    public void get_valid_type () throws EntityNotFoundException {        assertEquals("daily",dataServices.getTask(key.getName()).getProperty("type"));    }}
+package TodoModelServices;
+
+
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class TodoDataServicesTest {
+
+    private static final LocalServiceTestHelper helper =
+            new LocalServiceTestHelper(
+                    new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(0));
+
+    private static final TodoDataServices dataServices = new TodoDataServices();
+
+
+    @BeforeClass
+    public static void setup(){
+        helper.setUp();
+
+    }
+
+    @AfterClass
+    public static  void done (){
+        helper.tearDown();
+    }
+
+    @Test
+    public void saveTask_test_valid_parameter(){
+        Key key = dataServices.saveTask("daily","unique_id343434");
+        assertEquals("unique_id343434",key.getName());
+    }
+
+    @Test
+    public void getTask_test() throws EntityNotFoundException {
+         Entity entity = dataServices.getTask("unique_id343434");
+        assertEquals("unique_id343434",entity.getKey().getName());
+    }
+
+
+}
